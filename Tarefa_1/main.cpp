a53e724d78dd5f2bcd1af1aa51de99ce5f7f2b8d
LtavaresII <30603702+LtavaresII@users.noreply.github.com>
2020-03-02 08:00:53 -0300

    Add files via upload

---
+++ Tarefa_1/main.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void Ordenar(int *x, int *y, int*z);
+
+int main()
+{
+    int a = 7, b =6, c = 5;
+    printf("Antes de Ordenar:\n");
+    printf("a = %d, b = %d, c = %d\n", a, b, c);
+    Ordenar(&a, &b, &c);
+    return 0;
+}
+
+void Ordenar(int *x, int *y, int*z){
+    int tmp;
+
+    if(*x > *y){
+        // Primeiro e segundo
+        tmp = *x;
+        *x = *y;
+        *y = tmp;
+    }
+
+    if(*x > *z){
+        // Primeiro e ultimo
+        tmp = *x;
+        *x = *z;
+        *z = tmp;
+    }
+
+    if(*y > *z){
+        // Segundo e ultimo
+        tmp = *y;
+        *y = *z;
+        *z = tmp;
+    }
+
+    printf("Depois de Ordenar:\n");
+    printf("a = %d, b = %d, c = %d\n", *x, *y, *z);
+}
